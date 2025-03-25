f3d705eb8ac4c8f296ba8b713d58d3d85bf8c44f
nathalyharo <nathaly.haro98@gmail.com>
2025-03-25 16:16:14 -0600

    Made a small change to the env folder.
    Updated the key in env folder.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL,
-  process.env.EXPO_PUBLIC_SUPABASE_KEY
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default supabase;
